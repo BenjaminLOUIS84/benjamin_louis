fa58e6b9b0b0000de0c9a086931209ad8c86c0f4
Benjamin Louis <louisbenetval@orange.fr>
2023-03-27 00:36:02 +0200

    Mise à jour

---
+++ algoPhp/partie2/exo3.php
@@ -0,0 +1,17 @@
+<h1>Exercice 3</h1>
+
+<p>
+Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir 
+dans un nouvel onglet (_blank).
+</p>
+
+<h2>Résultat</h2>
+<a href="http://www.elan-formation.fr" target="_blank">Accéder au Site Web</a>
+
+<?php
+//      Pour afficher un lien hypertexte il faut écrire une balise a href en html
+//      Pour que la page s'ouvre dans un nouvel onglet il faut définir "_blank" comme cible
+?>
+
+
+
---
+++ algoPhp/partie2/exo4.php
@@ -0,0 +1,11 @@
+<h1>Exercice 4</h1>
+
+<p>
+
+</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+?>

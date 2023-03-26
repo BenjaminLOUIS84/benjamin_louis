04b0506791f68954ab91f7d9440759ac689016e5
Benjamin Louis <louisbenetval@orange.fr>
2023-03-26 17:31:29 +0200

    Mise à jour

--- algoPhp/partie1/exo11.php
+++ algoPhp/partie1/exo11.php
@@ -12,14 +12,15 @@ de voitures présentes dans le tableau.
      
 <?php
 
-//   Etape 1    Déclarer le tableau et les éléments texte contenu dans celui ci METHODE 1
+//   Etape 1    Déclarer le tableau et les éléments texte contenu dans celui ci
 
      $tableau = [" Peugeot "," Renault "," BMW "," Mercedes "];
      
-//                                                                              METHODE 2
+//              Autre facçon de déclarer un tableau
+
 //   $tableau = array("Peugeot","Renault","BMW","Mercedes");
 
-//    Etape 2   Pour compter le nombre d'éléments du tableau
+//   Etape 2    Pour compter le nombre d'éléments du tableau
 //              Utiliser la fonction de comptage:  count()
 
 //              $tailleTableau = count($tableau);
@@ -32,25 +33,41 @@ de voitures présentes dans le tableau.
 
      echo "<br>Il y a ".count($tableau)." marques de voitures dans ce tableau:<br>"; 
 
-//             Si on fait référence à la fonction plusieurs fois
-//             Délcarer la fonction de comptage (CF Etape 2)
-//             Ecrire echo comme ci dessous:
+//              Si on fait référence à la fonction plusieurs fois
+//              Délcarer la fonction de comptage (CF Etape 2)
+//              Ecrire echo comme ci dessous:
 
 //   echo "<br>Il y a " $tailleTableau " marques de voitures dans ce tableau:<br>";
 
-//   Pour afficher les caractéristiques du tableau utiliser: var_dump()               ASTUCE
+//   ASTUCE     Pour afficher les caractéristiques du tableau utiliser: var_dump()           
 
 //   echo var_dump($tableau);
 
 //   Etape 4    Pour afficher chaque ligne du tableau verticalement et avec une puce
 
-//   Mettre le tableau dans une liste
+//              Fonctions pour parcourir le tableau
 
-     $my_array = $tableau;
-     list($a,$b,$c,$d)= $my_array;
+//   METHODE 1  Considérer $tableau comme $my_array
+//              Utiliser la fonction list pour attribuer des lettres aux noms de marques
+//              Afficher les éléments de list avec des balises pour positionner verticalement
 
-     echo "<br>o $a<br> o $b<br> o $c<br> o $d<br>";
+//   $my_array = $tableau;           
+//   list($a,$b,$c,$d)= $my_array;
+//   echo "<br>o $a<br> o $b<br> o $c<br> o $d<br>";
 
+//   METHODE 2  Utiliser la fonction foreach pour designer toutes les valeurs du tableau
+//              Afficher ces valeurs avec un espace entre elles si à la suite ." "
+//              Afficher ces valeurs à la ligne avec ."<br>"
+//              
+     foreach($tableau as $value){echo $value."<br>";}
+
+//   METHODE 3  Utiliser les boucles  for ou while 
+//   pour afficher les éléments de premier au dernier
+  
+//   for($i = 0; $i < count($tableau); $i++){echo $tableau[$i]."<br>";}
+//   $i = 0;
+//   $nbMarques = count($tableau);
+//   while($i < $nbMarques){echo $tableau[$i]."<br>";$i++;}
 
 ?>
 

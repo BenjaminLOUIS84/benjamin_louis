b17d5a87569b22dbd180204959616844c2e29d27
Benjamin Louis <louisbenetval@orange.fr>
2023-03-27 20:08:00 +0200

    MAJ

--- algoPhp/partie2/exo8.php
+++ algoPhp/partie2/exo8.php
@@ -1,6 +1,8 @@
 <h1>Exercice 8</h1>
 
 <p>
+Soit l’URL suivante : http://my.mobirise.com/data/userpic/764.jpg
+Créer une fonction personnalisée permettant d’afficher l’image N fois à l’écran.
 </p>
 
 <h2>Résultat</h2>
@@ -8,20 +10,29 @@
 
 <?php
 
-//      Déclarer le tableau associatif
+//      Déclarer l'Url de l'image
+//      Utiliser base64_encode pour encoder les données et restituer l'image
+//      Utiliser file_get_contents pour récupérer les données brut de l'image
 
-$url = "http://my.mobirise.com/data/userpic/764.jpg";
+$url = 'http://my.mobirise.com/data/userpic/764.jpg';
+
+$imageData = base64_encode(file_get_contents($url));
+
+//echo '<img src = "data:image/jpeg;base64,'.$imageData.'">'; Pour afficher l'image
+
+//Déclarer le nombre de répétitions
+      
 $nbRepetition = 4;
 
 //      Ecrire la fonction de cette façon ci dessous
 
-function repeterImage($url,$nbRepetition){
+function repeterImage($imageData,$nbRepetition){
                    
     $resultat = "";
         foreach (range(1,$nbRepetition) as $value) {
-            $resultat .= $url;
+            $resultat .='<img src = "data:image/jpeg;base64,'.$imageData.'">';
        } 
     return $resultat;
 }
-echo repeterImage($url,$nbRepetition);
+echo repeterImage($imageData,$nbRepetition);
  ?>

6f5c37b334324b478f6fbdfcf2e29acea1a7ad36
Benjamin Louis <louisbenetval@orange.fr>
2023-03-29 01:25:26 +0200

    MAJ

--- algoPhp/partie2/exo11.php
+++ algoPhp/partie2/exo11.php
@@ -1,12 +1,32 @@
 <h1>Exercice 11</h1>
 
 <p>
-      
+Ecrire une fonction personnalisée qui affiche une date en français (en toutes lettres)
+à partir d’une chaîne de caractère représentant une date.
+Exemple :
+formaterDateFr("2018-02-23");     
 </p>
 
 <h2>Résultat</h2>
 
 
 <?php
+$date = "2018-02-23";
+    echo formaterDateFr($date);
+// Définition de la fonction
+function formaterDateFr($date){
 
+// Utilisation intldateformatter  
+$dateFr = new DateTime($date);  
+$fonctionDateFormat = new IntlDateFormatter(
+    "fr_FR",
+    IntlDateFormatter::FULL,//ajoute le jour de la semaine
+    IntlDateFormatter::NONE,//supprime les heure après
+    'Europe/Paris', 
+    IntlDateFormatter::GREGORIAN  ); 
+
+//Mise en forme de la date 
+    $fonctionDateFormat->setPattern('EEEE dd MMMM YYYY');  //ce pattern crée le format (jours date mois année)
+    return $fonctionDateFormat->format($dateFr);
+};
 ?>
---
+++ algoPhp/partie2/exo15.php
@@ -0,0 +1,17 @@
+<h1>Exercice 15</h1>
+
+<p>
+En utilisant les ressources de la page http://php.net/manual/fr/book.filter.php,
+vérifier si une adresse e-mail a le bon format.
+Affichage
+L’adresse elan@elan-formation.fr est une adresse e-mail valide
+L’adresse contact@elan est une adresse e-mail invalid
+</p>
+
+<h2>Résultat</h2>
+
+
+<?php
+
+
+?>

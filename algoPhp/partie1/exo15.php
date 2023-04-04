8862dc100bd8b2237d7e695629eea7de6046a1e6
Benjamin Louis <louisbenetval@orange.fr>
2023-04-04 15:49:03 +0200

    MAJ

--- algoPhp/partie1/exo15.php
+++ algoPhp/partie1/exo15.php
@@ -8,20 +8,37 @@ $p2 = new Personne("DUCHEMIN","Alice","1985-01-17");
 </P>
 
 <h2>Résultat</h2>
-
+<!-- 
+CREER UNE CLASS Personne (Commence toujours par une majuscule)
+    
+    1   Déclarer les variables publique pour y accéder facilement
+    2   Déclarer la fonction __construct() et instancier les variables
+    3   Déclarer une fonction calculerAge 
+ -->
 <?php
 class Personne{
 
+  //Définition des propriétés de la class Personne
+        //Propriétés(Variables) de la class Personne qui stock le nom de la personne
+      
     public string $nom;
     public string $prenom;
     public DateTime $date;
+
+ //METHODE MAGIQUE: Magic Method Constructor
+        //Cette méthode est annoncée par __ pour désigner les méthodes à employer
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public function __construct(string $nom, string $prenom, string $date){
         $this->nom = $nom;
         $this->prenom = $prenom;
         $this->date = new DateTime($date);
     }
-
+ //Entre les {} se trouvent les arguments utilsés pour paramêtrer à l'instanciation
+        //(Créer un objet dans la mémoire de l'ordinateur) Utiliser $this pour désigner l'objet et accéder à sa valeur
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ //Créer une nouvelle fonction pour utiliser dans une phrase les valeurs des variables par concatenation
+  
     public function calculerAge(){
         $now = new DateTime();
         $dateNaiss = $this->date;
@@ -29,6 +46,7 @@ class Personne{
         $difference = date_diff($dateNaiss, $now);
         return $difference->y;
     }
+//METHODE function __toString permet de retourner un objet sous la fome d'une chaîne de caractères
     
     public function __toString(){
         return $this->nom." ".$this->prenom." a ".$this->calculerAge()." ans.<br>";
@@ -36,10 +54,13 @@ class Personne{
 
 }
 
+//Créer(Déclarer) les animaux
+
 $p1 = new Personne("DUPONT","Michel","1980-02-19");
 $p2 = new Personne("DUCHEMIN","Alice","1985-01-17");
 
-
+//Faire appel à la méthode pour attribuera les bonnes valeurs aux propriétés des personnes
+        
 //var_dump($p1);
 
 echo"<br>";
---
+++ algoPhp/partie2/exo13.php
@@ -0,0 +1,13 @@
+<h1>Exercice 13</h1>
+
+<p>
+Créer une classe Voiture possèdant les propriétés suivantes: marques, modèle, nbPortes et vitesseActuelle
+ainsi que les méthodes demarrer(), accelerer() et stopper() en plus des accesseurs (get) et mutateurs (set) traditionnels. 
+La vitesse initiale de chaque véhicule instancié est de 0.
+Une méthode personnalisée pourra afficher toutes les informations d'un véhicule.
+</p>
+
+<?php
+
+
+?>

7adb6d046b154db1e559daadb7cd561a9b4c0caf
Benjamin Louis <louisbenetval@orange.fr>
2023-04-11 17:17:05 +0200

    MAJ

--- algoPhp/partie3/exoBank.php
+++ algoPhp/partie3/exoBank.php
@@ -32,14 +32,61 @@ Afficher toutes les informations d'un compte bancaire, notamment le nom / préno
 
 <?php
 
+class Titulaire {
+
+    private string $nom;
+    private string $prenom;
+    private DateTime $date;
+    private string $ville;
+
+    public function __construct($nom, $prenom, $date, $ville){
+        $this->nom = $nom;  
+        $this->prenom = $prenom;
+        $this->date = new DateTime($date);
+        $this->ville = $ville;
+    }
+
+    public function calculerAge(){
+        $now = new DateTime();
+        $dateNaissance = $this->date;
+    
+        $difference = date_diff($dateNaissance, $now);
+        return $difference->y;
+
+    }
+
+    public function __toString(){
+        return $this->nom." ".$this->prenom." a ".$this->calculerAge()." ans et habite à " .$this->ville."<br>";
+    }
 
 
+}
+
+$personne1 = new Titulaire("NEMAR", "Jean", "1980-02-19", "Colmar");
+echo $personne1;
 
 
+class Compte extends Titulaire {
 
+    private string $label;
+    private int $soldeInit;
+    private string $devise;
+    
 
+    public function __construct($label, $soldeInit, $devise){
+        $this->label = $label;
+        $this->soldeInit = $soldeInit;
+        $this->devise = $devise;
+    }
 
+    public function __toString(){
+        return "Le ".$this->label." présente un solde de " .$this->soldeInit." ".$this->devise. "<br>";
+    }
+    
+}
 
+$compte1 = new Compte("Compte Courant",200,"euros");
+echo $compte1;
 
 
 ?>

d28dd6b9a431f0653a8f91f9a6a0379d60772507
Benjamin Louis <louisbenetval@orange.fr>
2023-04-11 18:10:08 +0200

    MAJ

--- algoPhp/partie3/exoBank.php
+++ algoPhp/partie3/exoBank.php
@@ -38,12 +38,14 @@ class Titulaire {
     private string $prenom;
     private DateTime $date;
     private string $ville;
+    private string $Compte;
 
-    public function __construct($nom, $prenom, $date, $ville){
+    public function __construct($nom, $prenom, $date, $ville, $compte){
         $this->nom = $nom;  
         $this->prenom = $prenom;
         $this->date = new DateTime($date);
         $this->ville = $ville;
+        $this->compte = $compte;
     }
 
     public function calculerAge(){
@@ -56,36 +58,66 @@ class Titulaire {
     }
 
     public function __toString(){
-        return $this->nom." ".$this->prenom." a ".$this->calculerAge()." ans et habite à " .$this->ville."<br>";
+        return $this->nom." ".$this->prenom." a ".$this->calculerAge()." ans et habite à " 
+        .$this->ville." et dispose d'un ".$this->compte."<br>";
     }
 
 
 }
 
-$personne1 = new Titulaire("NEMAR", "Jean", "1980-02-19", "Colmar");
+$personne1 = new Titulaire("NEMAR", "Jean", "1980-02-19", "Colmar","Compte Courant");
 echo $personne1;
+echo"<br>";
 
-
-class Compte extends Titulaire {
+class Compte {
 
     private string $label;
     private int $soldeInit;
     private string $devise;
+    private string $titulaire;
+    // private bool $crediter;
+    // private bool $debiter;
+    //private bool $virer
     
 
-    public function __construct($label, $soldeInit, $devise){
+    public function __construct($label, $soldeInit, $devise, $titulaire, 
+        //$crediter=false, $debiter= false, $virer=false
+        ){
         $this->label = $label;
         $this->soldeInit = $soldeInit;
         $this->devise = $devise;
+        $this->titulaire = $titulaire;
+        // $this->crediter = $crediter;
+        // $this->debiter = $debiter;
+        // $this->virer = $virer;
     }
 
     public function __toString(){
-        return "Le ".$this->label." présente un solde de " .$this->soldeInit." ".$this->devise. "<br>";
+        return "Le ".$this->label." de ".$this->titulaire." présente un solde de " 
+        .$this->soldeInit." ".$this->devise. "<br>";
     }
     
+    // public function crediter(){
+    //     if($soldeInit){
+    //         echo "<p>Le" .$this->label. " est crédité de X euros</p>";
+    //     }else{
+    //         $this->isStart= false;
+    //         echo "<p>Le" .$this->label. " n'est pas crédité de X euros</p>";
+    //     }
+    // }
+
+    // public function debiter(){
+        
+    // }
+
+    // public function virer(){
+        
+    // }
+
+
 }
 
-$compte1 = new Compte("Compte Courant",200,"euros");
+$compte1 = new Compte("Compte Courant",200,"euros","NEMAR Jean");
 echo $compte1;
 
 

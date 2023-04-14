eac70329b4e33362c9aa28eff628feb4fa9ee713
Benjamin Louis <louisbenetval@orange.fr>
2023-04-14 16:01:35 +0200

    MAJ

--- algoPhp/correction/index.php
+++ algoPhp/correction/index.php
@@ -10,6 +10,34 @@
 </head>
 
 <body>
+<h1>Exercice</h1>
+
+<p>
+Vous êtes chargé(e) de créer un projet orienté objet<br>
+permettant de gérer des titulaires et leurs comptes bancaires respectifs.<br>
+Un compte bancaire est composé des éléments suivants :<br>
+<br>
+Un libellé (compte courant, livret A, ...)<br>
+Un solde initial<br>
+Une devise monétaire<br>
+Un titulaire unique<br>
+<br>
+Un titulaire comporte :<br>
+Un nom<br>
+Un prénom<br>
+Une date de naissance<br>
+Une ville<br>
+L'ensemble de ses comptes bancaires.<br>
+<br>
+Sur un compte bancaire, on doit pouvoir :<br>
+Créditer le compte de X euros<br>
+Débiter le compte de X euros<br>
+Effectuer un virement d'un compte à l'autre.<br>
+<br>
+On doit pouvoir :<br>
+Afficher toutes les informations d'un(e) titulaire (dont l'âge) et l'ensemble des comptes appartenant à celui(celle)-ci.<br>
+Afficher toutes les informations d'un compte bancaire, notamment le nom / prénom du titulaire du compte.<br>
+</p>
 
 
 <?php

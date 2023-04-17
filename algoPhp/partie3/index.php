f4e4ef446bc10d40e2987c55f5d73051d028e98a
Benjamin Louis <louisbenetval@orange.fr>
2023-04-17 09:03:32 +0200

    MAJ

---
+++ algoPhp/partie3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta charset="utf-8">  <link rel="stylesheet" href="style.css">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livre</title>
+</head>
+
+<body>
+<h1>Exercice</h1>
+<p>Vous êtes chargé(e) de créer un projet orienté objet permettant de gérer des livres et leurs auteurs respectifs.<br>
+Les livres sont caractérisés par un titre, un nombre de pages, une année de parution, un prix et un auteur.<br>
+Un auteur comporte un nom et un prénom.Une méthode toString() sera appréciée dans chacune de vos classes.<br>
+Vous implémenterez une méthode afficherBibliographie() qui permettra d’afficher la bibliographie complète d’un auteur :<br>
